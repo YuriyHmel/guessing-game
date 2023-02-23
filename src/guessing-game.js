442687188c62d2905d7e7ca4a254964f6bc9a587
YuriyHmel <hmelnich@yandex.ru>
2023-02-23 19:08:46 +0600

    feat: make task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.avgValue = Math.round((this.max - this.min) / 2);
     }
 
     guess() {
-
+        console.log(this.avgValue)
+        return this.avgValue;
     }
 
     lower() {
-
+        this.max = this.avgValue;
+        this.avgValue = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.avgValue;
+        this.avgValue = Math.round((this.max + this.min) / 2);
     }
 }
 
